33443846a9c4e45e864e0db9a45a5fe055780bf0
Chaitanya Patel <chaitanya100100@gmail.com>
2018-11-03 00:07:30 +0530

    refactoring and report

--- src/two_phase_merge_sort.cpp
+++ src/two_phase_merge_sort.cpp
@@ -24,6 +24,7 @@ long long MM; // main memory size
 long long R; // record size
 long long NUMR; // total number of records
 long long NUMSUB; // number of sublists
+vector<long long> SUBLEN;
 bool ASC;
 
 struct Record
@@ -108,6 +109,14 @@ void preprocess(int argc, char ** argv)
     }
     metafile.close();
 
+    // get record size from meta file
+    // make sure to take care of delimeter size after each record and
+    // ending of each record(either '\n' or '\r\n')
+    long long meta_rsz = 0;
+    for(int i = 0; i < col_lengths.size(); i++)
+        meta_rsz += col_lengths[i];
+    meta_rsz += col_lengths.size()*2;
+
     if(argc < 6)
     {
         cerr << "Not enough args" << endl;
@@ -152,15 +161,22 @@ void preprocess(int argc, char ** argv)
     long long file_size = inpf.tellg();
     inpf.seekg(0, inpf.beg);
 
-    inpf.unsetf(ios_base::skipws);
-    long long line_count = count(istream_iterator<char>(inpf), istream_iterator<char>(), '\n');
-    inpf.close();
+    // inpf.unsetf(ios_base::skipws);
+    // long long line_count = count(istream_iterator<char>(inpf), istream_iterator<char>(), '\n');
+    // inpf.close();
+    // if(file_size % line_count != 0)
+    // {
+    //     cerr << "File size is not a multiple of each line size" << endl;
+    //     exit(-1);
+    // }
 
-    if(file_size % line_count != 0)
+    if(file_size % meta_rsz != 0)
     {
-        cerr << "File size is not a multiple of each line size" << endl;
+        cerr << "File size is not a multiple of each record size" << endl;
         exit(-1);
     }
+    long long line_count = file_size / meta_rsz;
+
     NUMR = line_count;
     R = file_size / line_count;
 
@@ -173,10 +189,28 @@ void preprocess(int argc, char ** argv)
     cout << "MM : " << MM << endl;
     cout << "NUMR : " << NUMR << endl;
     cout << endl;
+
+    if(MM * MM < NUMR)
+    {
+        cerr << "Too many records for two way merge sort, need K-way(K > 2) merge sort" << endl;
+        exit(-1);
+    }
     // for(int i = 0; i < col_names.size(); i++)
     //     cout << col_names[i] << " " << col_lengths[i] << endl;
     // for(int i = 0; i < sort_col_idx.size(); i++)
     //     cout << sort_col_idx[i] << endl;
+}
+
+Record construct_record(string & line)
+{
+    vector<string> vs(num_cols);
+    long long pos = 0, off = 0;
+    for(int i = 0; i < num_cols; i++)
+    {
+        vs[i] = line.substr(off, col_lengths[i]);
+        off = line.find_first_not_of(" ", off + col_lengths[i]);
+    }
+    return Record(vs);
 }
 
 void write_records(ofstream & of, vector<Record> & records)
@@ -192,9 +226,10 @@ void write_records(ofstream & of, vector<Record> & records)
         // cout << s;
         // of << s << endl;
     }
+    of.flush();
 }
 
-void write_records2(ofstream & of, vector<Record> & records)
+void write_records_chunk(ofstream & of, vector<Record> & records)
 {
     string s;
     vector<string> vs(records.size());
@@ -213,24 +248,29 @@ void write_records2(ofstream & of, vector<Record> & records)
     std::stringstream fin;
     copy(vs.begin(), vs.end(), ostream_iterator<string>(fin, "\r\n"));
     of << fin.rdbuf();
+    of.flush();
 }
 
-Record construct_record(string & line)
+void read_records_chunk(ifstream & ifs, long long n, vector<Record> & records)
 {
-    vector<string> vs(num_cols);
-    long long pos = 0, off = 0;
-    for(int i = 0; i < num_cols; i++)
+    char * buf = new char[n*R+1];
+    ifs.read(buf, n*R);
+    buf[n*R] = '\0';
+    // cout << buf << endl;
+
+    stringstream ss(buf);
+    string line;
+    while(getline(ss, line))
     {
-        vs[i] = line.substr(off, col_lengths[i]);
-        off = line.find_first_not_of(" ", off + col_lengths[i]);
+        // cout << line << endl;
+        records.push_back(construct_record(line));
     }
-    return Record(vs);
+    delete buf;
 }
+
 
 void phase_one()
 {
-    // ideally it should be MM but there is other constant space requirement for this code
-    // so I am taking MM/2
     long long MAXN = MM;
     cout << "Phase 1 started" << endl;
     cout << "MAXN : " << MAXN << endl;
@@ -273,10 +313,53 @@ void phase_one()
     cout << endl;
 }
 
+
+void phase_one_chunk()
+{
+    long long MAXN = MM;
+    cout << "Phase 1 started" << endl;
+    cout << "MAXN : " << MAXN << endl;
+
+    ifstream inpf(input_path);
+    if(!inpf.good())
+    {
+        cerr << "Unable to open input file" << endl;
+        exit(-1);
+    }
+
+    NUMSUB = 0;
+    while(true)
+    {
+        long long sz = min(MAXN, NUMR-NUMSUB*MAXN);
+        if(sz <= 0) break;
+        vector<Record> records;
+        read_records_chunk(inpf, sz, records);
+
+        phase1sort(records);
+        string inter_outpath = inter_outdir + to_string(NUMSUB) + ".txt";
+        ofstream inter_outf(inter_outpath);
+        if(!inter_outf.good())
+        {
+            cerr << "Unable to open inter out file : " << inter_outpath<< endl;
+            exit(-1);
+        }
+        SUBLEN.push_back(records.size());
+        write_records_chunk(inter_outf, records);
+        inter_outf.close();
+        records.clear();
+        NUMSUB++;
+    }
+
+    inpf.close();
+    cout << "NUMSUB : " << NUMSUB << endl;
+    cout << endl;
+}
+
+
 void phase_two()
 {
     cout << "Phase 2 started" << endl;
-    long long H = MM / (1 + NUMSUB);
+    long long H = (long long)(MM / (1 + sqrt(NUMSUB)));
     long long SUBSZ = (MM - H) / NUMSUB;
 
 
@@ -371,10 +454,110 @@ void phase_two()
     cout << endl;
 }
 
+
+void phase_two_chunk()
+{
+    cout << "Phase 2 started" << endl;
+    // long long H = (long long)(MM / (1 + sqrt(NUMSUB)));
+    long long H = MM / (1 + NUMSUB);
+    long long SUBSZ = (MM - H) / NUMSUB;
+
+
+    cout << "NUMSUB : " << NUMSUB << endl;
+    cout << "SUBSZ : " << SUBSZ << endl;
+    cout << "H : " << H << endl;
+
+    ofstream outf(output_path);
+    if(!outf.good())
+    {
+        cerr << "Unable to open output file" << endl;
+        exit(-1);
+    }
+    ifstream inter_outf[NUMSUB];
+    for(int i = 0; i < NUMSUB; i++)
+    {
+        string inter_outpath = inter_outdir + to_string(i) + ".txt";
+        inter_outf[i].open(inter_outpath);
+        if(!inter_outf[i].good())
+        {
+            cerr << "Unable to open inter out file : " << inter_outpath << endl;
+            exit(-1);
+        }
+    }
+
+    vector<Record> subl[NUMSUB];
+    vector<long long> readidx(NUMSUB, 0LL);
+    string line;
+    for(int i = 0; i < NUMSUB; i++)
+    {
+        long long sz = min(SUBSZ, SUBLEN[i] - readidx[i]);
+        read_records_chunk(inter_outf[i], sz, subl[i]);
+        readidx[i] += sz;
+    }
+    vector<long long> sublidx(NUMSUB, 0);
+
+    priority_queue<pair<Record, int>, vector<pair<Record, int> >, compare_phase2> pq;
+
+    for(int i = 0; i < NUMSUB; i++)
+        for(int j = 0; j < H/NUMSUB && j < subl[i].size(); j++)
+        {
+            pq.push({subl[i][j], i});
+            sublidx[i]++;
+        }
+
+    vector<Record> out_records;
+    while(!pq.empty())
+    {
+        pair<Record, int> ri = pq.top(); pq.pop();
+        out_records.push_back(ri.first);
+
+        if(out_records.size() >= H)
+        {
+            write_records_chunk(outf, out_records);
+            out_records.clear();
+        }
+
+        int si = ri.second;
+        if(sublidx[si] == subl[si].size())
+        {
+            long long sz = min(SUBSZ, SUBLEN[si] - readidx[si]);
+            if(sz)
+            {
+                subl[si].clear();
+                read_records_chunk(inter_outf[si], sz, subl[si]);
+                readidx[si] += sz;
+
+                sublidx[si] = 0;
+                pq.push({subl[si][0], si});
+                sublidx[si]++;
+            }
+        }
+        else
+        {
+            pq.push({subl[si][sublidx[si]], si});
+            sublidx[si]++;
+        }
+    }
+    if(out_records.size())
+    {
+        write_records_chunk(outf, out_records);
+        out_records.clear();
+    }
+    for(int i = 0; i < NUMSUB; i++)
+        inter_outf[i].close();
+    outf.close();
+    cout << endl;
+}
+
+
+
 int main(int argc, char **argv)
 {
     preprocess(argc, argv);
-    phase_one();
-    phase_two();
+    // phase_one();
+    phase_one_chunk();
+
+    // phase_two();
+    phase_two_chunk();
     return 0;
 }
